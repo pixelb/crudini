27d1503b4c0bc17aa60beba37121a9f4be905527
Pádraig Brady <P@draigBrady.com>
2014-09-05 17:22:38 +0100

    add a setup.py to support pip install
    Fixes issue #9

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "crudini",
+    version = "0.4",
+    author = "Pádraig Brady",
+    author_email = "P@draigBrady.com",
+    description = ("A utility for manipulating ini files"),
+    license = "GPLv2",
+    keywords = "ini config edit",
+    url = "http://github.com/pixelb/crudini",
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Topic :: System :: Systems Administration",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 2",
+    ],
+    install_requires = ['iniparse'],
+    scripts=["crudini"]
+)
