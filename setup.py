100a48af047eb5af465e83e5a2970f3521863941
KOLANICH <kolan_n@mail.ru>
2021-10-11 12:20:20 +0300

    Moved all the metadata into setup.cfg. Added pyproject.toml. Version info within package metadata is now maintained via setuptools_scm. Fixed the obsolete way of providing binaries. `scripts` must not be used, it is not Windows-compatible. The `console-scripts` entry point is (`pip` will generate a real PE exe on installation triggering right package importing and execution when the exe is launched).

--- setup.py
+++ setup.py
@@ -1,32 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
 from setuptools import setup
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    name="crudini",
-    version="0.9.3",
-    author="Pádraig Brady",
-    author_email="P@draigBrady.com",
-    description=("A utility for manipulating ini files"),
-    license="GPLv2",
-    keywords="ini config edit",
-    url="http://github.com/pixelb/crudini",
-    long_description="```\n" + read('README') + "```\n",
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Utilities",
-        "Topic :: System :: Systems Administration",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-    ],
-    install_requires=['iniparse>=0.5'],
-    scripts=["crudini"]
-)
+if __name__ == "__main__":
+    setup()
